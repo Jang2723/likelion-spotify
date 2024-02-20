955eebfa74ac804a0fb451e172558a92f17dbb48
Jang2723 <jangsb46@gmail.com>
2024-02-20 12:06:34 +0900

    spotify api to retrieve entity info

--- src/main/java/com/example/music/SpotifyTokenService.java
+++ src/main/java/com/example/music/SpotifyTokenService.java
@@ -16,35 +16,31 @@ import java.util.Map;
 @Slf4j
 @Component
 public class SpotifyTokenService {
-//    @Value("${spotify.client-id}")
-//    private String clientId;
-//    @Value("${spotify.client-secret}")
-//    private String clientSecret;
-
-    // Request Body가 변하지 않으므로 필드로 올리자.
-    private final MultiValueMap<String , Object> parts;
+    // Request Body가 변하지 않음으로 필드로 올리자.
+    private final MultiValueMap<String, Object> parts;
 
     // 마지막으로 Token을 발급한 시점
     private LocalDateTime lastIssued;
 
-    // 현재 사용 중인 Bearer Token
+    // 현재 사용중인 Bearer Token
     private String token;
     private final RestClient authRestClient;
 
     public SpotifyTokenService(
-            RestClient authRestClient,
             @Value("${spotify.client-id}")
             String clientId,
             @Value("${spotify.client-secret}")
             String clientSecret
     ) {
-        this.authRestClient = authRestClient;
-        // 항상 같은 Request Body를 보내게 되므로,
-        // 해당 Request Body는 저장을 해두자
+        this.authRestClient = RestClient.builder()
+                .baseUrl("https://accounts.spotify.com/api/token")
+                .build();
+        // 항상 같은 Request Body를 보내게 됨으로,
+        // 해당 Request Body는 저장을 해두자.
         this.parts = new LinkedMultiValueMap<>();
-        this.parts.add("grant_type","client_credentials");
-        this.parts.add("client_id",clientId);
-        this.parts.add("client_secret",clientSecret);
+        this.parts.add("grant_type", "client_credentials");
+        this.parts.add("client_id", clientId);
+        this.parts.add("client_secret", clientSecret);
 
         reissue();
     }
@@ -52,9 +48,9 @@ public class SpotifyTokenService {
     // Token을 재발행하는 메서드
     private void reissue() {
         log.info("issuing access token");
-        // 1. Token을 발행 받는다
+        // 1. Token을 발행받는다.
         AccessTokenDto response = authRestClient.post()
-                // Content-Type : 헤더 설정
+                // Content-Type 헤더 설정
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(parts)
                 .retrieve()
@@ -71,24 +67,19 @@ public class SpotifyTokenService {
     public String getToken() {
         log.info("last issued: {}", lastIssued);
         log.info("time passed: {} mins", ChronoUnit.MINUTES.between(lastIssued, LocalDateTime.now()));
-        // 만약 마지막에 발급받은지 50분이 지났다.
+        // 만약 마지막에 발급받은지 50분이 지났다
         if (lastIssued.isBefore(LocalDateTime.now().minusMinutes(50)))
-            // 그런 경우 재발행
-            reissue();;
-        // 재발행을 했든 안했뜬 token을 돌려준다.
+            // 그럴경우 재발행
+            reissue();
+        // 재발행을 했든 안했든 token을 돌려준다.
         return token;
     }
 
     // 메서드가 실행될 때 마다 AccessToken이 재발행 된다.
     @Deprecated
     public AccessTokenDto getAccessToken() {
-//        MultiValueMap<String ,Object> parts = new LinkedMultiValueMap<>();
-//        parts.add("grant_type","client_credentials");
-//        parts.add("client_id",clientId);
-//        parts.add("client_secret",clientSecret);
-
         return authRestClient.post()
-                // Content-Type : 헤더 설정
+                // Content-Type 헤더 설정
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .body(parts)
                 .retrieve()
