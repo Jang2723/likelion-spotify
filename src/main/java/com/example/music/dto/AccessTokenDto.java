d14bcfdec759f556a7a0e5f76d811fc3db72155c
Jang2723 <jangsb46@gmail.com>
2024-02-20 10:11:38 +0900

    init

--- src/main/java/com/example/music/dto/AccessTokenDto.java
+++ src/main/java/com/example/music/dto/AccessTokenDto.java
@@ -1,11 +1,11 @@
 package com.example.music.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
 @Data
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class AccessTokenDto {
     private String accessToken;
     private String tokenType;
